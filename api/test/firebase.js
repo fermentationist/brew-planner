a3e14ae208842289281ee95d04822fbb5167c66a
Dennis Hodges <fermentationist@gmail.com>
2023-09-29 09:23:23 -0500

    Using VITE env variables in firebaseConfig

--- api/test/firebase.js
+++ api/test/firebase.js
@@ -4,12 +4,12 @@ import {getAuth, signInWithEmailAndPassword, signOut, setPersistence, inMemoryPe
 export {createUser, deleteUser, updateUser, getUser, getAllUsers} from "../src/services/user.js";
 
 const clientApp = initializeClientApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_API_ID,
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_API_ID,
 });
 
 const clientAuth = getAuth(clientApp);
